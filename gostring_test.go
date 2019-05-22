604c964a5e0fb431a7992b6e2bf71840465cdaff
arkady <larytet@yahoo.com>
2019-05-22 18:30:26 +0300

    Refactor

--- gostring_test.go
+++ gostring_test.go
@@ -9,7 +9,7 @@ func TestNonzero(t *testing.T) {
 	var data [4]uint8 = [4]uint8{0x30, 0x31, 0x32, 0x00}
 	s := GoString(unsafe.Pointer(&data))
 	if s != "012" {
-		t.Errorf("Failed to convert %s instead of %s", s, "012")
+		t.Errorf("Failed to convert '%s' instead of '%s'", s, "012")
 	}
 }
 
