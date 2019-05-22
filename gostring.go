dfb9d16c283b75f6e7a99c641b10cdd28a1f87bf
arkady <larytet@yahoo.com>
2019-05-22 18:22:44 +0300

    Refactor

--- gostring.go
+++ gostring.go
@@ -5,17 +5,21 @@ import (
 	"unsafe"
 )
 
-// TODO I can do better
-func GoString(data unsafe.Pointer) string {
+func StrLen(data unsafe.Pointer) int {
 	p := uintptr(data)
 	c := *(*uint8)(unsafe.Pointer(p))
 	for c != 0 {
 		p++
 		c = *(*uint8)(unsafe.Pointer(p))
 	}
+	return int(p - uintptr(data))
+}
+
+// TODO I can do better
+func GoString(data unsafe.Pointer) string {
 	sh := &reflect.StringHeader{
 		Data: uintptr(data),
-		Len:  int(p - uintptr(data)),
+		Len:  StrLen(data),
 	}
 	return *((*string)(unsafe.Pointer(sh)))
 }
