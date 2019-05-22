8c3c10a40db25dedf7fc2536259c301693cfd095
arkady <larytet@yahoo.com>
2019-05-22 18:26:01 +0300

    Refactor

--- gostring.go
+++ gostring.go
@@ -5,6 +5,7 @@ import (
 	"unsafe"
 )
 
+// TODO I can do better
 func StrLen(data unsafe.Pointer) int {
 	p := uintptr(data)
 	c := *(*uint8)(unsafe.Pointer(p))
@@ -15,7 +16,11 @@ func StrLen(data unsafe.Pointer) int {
 	return int(p - uintptr(data))
 }
 
-// TODO I can do better
+// GoString typecast pointer to go.string
+// The function does not allocate memory. Make sure
+// that the reference data remains valid through the
+// life cycle of the resulting sting
+// TODO causes memoty leak? How does it work?
 func GoString(data unsafe.Pointer) string {
 	sh := &reflect.StringHeader{
 		Data: uintptr(data),
