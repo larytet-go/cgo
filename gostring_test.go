35dfeda17e1d4a4660b7a54282f615a69c344099
arkady <larytet@yahoo.com>
2019-05-22 18:13:13 +0300

    Add unitest

--- gostring_test.go
+++ gostring_test.go
@@ -5,10 +5,18 @@ import (
 	"unsafe"
 )
 
-func TestPadding(t *testing.T) {
+func TestNonzero(t *testing.T) {
 	var data [4]uint8 = [4]uint8{0x30, 0x31, 0x32, 0x00}
 	s := GoString(unsafe.Pointer(&data))
 	if s != "012" {
 		t.Errorf("Failed to convert %s instead of %s", s, "012")
+	}
+}
+
+func TestZero(t *testing.T) {
+	var data [1]uint8 = [1]uint8{0x00}
+	s := GoString(unsafe.Pointer(&data))
+	if s != "" {
+		t.Errorf("Failed to convert empty string, got %s", s)
 	}
 }
